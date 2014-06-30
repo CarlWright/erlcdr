eb46542823060e0141f4ab25591987ffedbc7246
Carl A Wright <wright@servicelevel.net>
2014-06-30 18:58:22 -0400

    Use emacs pretty print to improve appearance

--- erlcdr.erl
+++ erlcdr.erl
@@ -6,20 +6,20 @@
 init() -> OFL = io:get_line("Output Filename: "),
           OFL2 = lists:sublist( OFL , length(OFL) - 1),
           OFL3 =  file:open(OFL2, [write]),
-           case  OFL3 of
+	  case  OFL3 of
               {ok, Outfile}   -> 
-                    IFL = io:get_line("Input Filename: "),
-                    Filename = lists:sublist( IFL , length(IFL) - 1),
-                    io:format("Start at ~w~n",[time()]),
-                    case  file:open(Filename, [raw, binary, read]) of
-                        {ok, Infile}    -> process( {Infile, Outfile}, file:read(Infile, 2048));
-                        {error, Reason} -> io:format(" Problem with input file: ~s~n",[file:format_error(Reason)]),
-                                            exit(Reason)
-                    end,
-                    ok;
-                {error, Reason} -> io:format(" Problem with output file: ~s~n",[file:format_error(Reason)]),
-                                    exit(Reason)
-            end.
+		  IFL = io:get_line("Input Filename: "),
+		  Filename = lists:sublist( IFL , length(IFL) - 1),
+		  io:format("Start at ~w~n",[time()]),
+		  case  file:open(Filename, [raw, binary, read]) of
+		      {ok, Infile}    -> process( {Infile, Outfile}, file:read(Infile, 2048));
+		      {error, Reason} -> io:format(" Problem with input file: ~s~n",[file:format_error(Reason)]),
+					 exit(Reason)
+		  end,
+		  ok;
+	      {error, Reason} -> io:format(" Problem with output file: ~s~n",[file:format_error(Reason)]),
+				 exit(Reason)
+	  end.
 
 %% main processing loop for the entire file.
 %%
@@ -35,29 +35,29 @@ process( {F,Out},{error, Reason} )  ->  file:close(F),
 
 %% break a block into the space holding records.
 parse_block( Block, Out ) ->
-  <<BDW:16, _:16, Rest/binary>> = Block,
-  {Remainder, _} = lists:split((BDW - 4), binary_to_list(Rest)),
-  record_parse(list_to_binary(Remainder), Out).
- 
+    <<BDW:16, _:16, Rest/binary>> = Block,
+    {Remainder, _} = lists:split((BDW - 4), binary_to_list(Rest)),
+    record_parse(list_to_binary(Remainder), Out).
+
 %% break the space holding records into individual records.  
 record_parse(<<>>, _Out) -> 0;
 record_parse( Records, Out) ->
-  <<RDW:16, _:16, Rest/binary>> = Records,
-  {Rec, OtherRecs} = lists:split( (RDW - 4), binary_to_list(Rest) ),
-  process_record(Rec, Out),
-  record_parse(list_to_binary(OtherRecs), Out).
+    <<RDW:16, _:16, Rest/binary>> = Records,
+    {Rec, OtherRecs} = lists:split( (RDW - 4), binary_to_list(Rest) ),
+    process_record(Rec, Out),
+    record_parse(list_to_binary(OtherRecs), Out).
 
 %% process a record 
 process_record(Record, Out) ->
-  CDR = bcd_to_ascii(Record), 
-  gsmcdr:process(CDR, Out).
+    CDR = bcd_to_ascii(Record), 
+    gsmcdr:process(CDR, Out).
 
 %% take a binary of BCD characters and get them into an ASCII list.  
 bcd_to_ascii([]) -> [];
 bcd_to_ascii(String) -> 
-BCD1 = << <<X:8>> || <<X:4>> <= list_to_binary(String) >>,
-BCD = binary_to_list(BCD1),
-asciiShift( BCD).
+    BCD1 = << <<X:8>> || <<X:4>> <= list_to_binary(String) >>,
+    BCD = binary_to_list(BCD1),
+    asciiShift( BCD).
 
 %% take BCD in 8 bit bytes and move them to visible ASCII values.
 asciiShift([H|T]) -> [case H < 10 of  true  -> H + 48;  false -> H + 55  end|asciiShift(T)];
--- gsmcdr.erl
+++ gsmcdr.erl
@@ -3,81 +3,80 @@
 -vsn(0.9).
 
 init (Outfile) ->
-  file:open(Outfile, [write]).
-  
+    file:open(Outfile, [write]).
+
 process(Rec, Out)  ->
-  ModuleFlag = case lists:sublist(Rec, 9,1) of
-    "0" -> false;
-    "1" -> true
-  end,
-  case lists:sublist(Rec, 10, 4) of
-   "0001" -> cdrStrip(locationUpdate, Rec, ModuleFlag, 256, false, Out);
-   "0002" -> cdrStrip(mobileOriginated, Rec, ModuleFlag, 392, true, Out);
-   "0003" -> cdrStrip(mobileTerminated, Rec, ModuleFlag, 358, true, Out);
-   "0004" -> cdrStrip(smsMobileOriginated, Rec, ModuleFlag, 302, true, Out);
-   "0005" -> cdrStrip(smsMobileTerminated, Rec, ModuleFlag, 276, true, Out);
-   "0006" -> cdrStrip(supplementaryServiceAction, Rec, ModuleFlag, 246, false, Out);
-   "0007" -> cdrStrip(tranferIn, Rec, ModuleFlag, 78, false, Out);
-   "0008" -> cdrStrip(transferOut, Rec, ModuleFlag, 98, false, Out);
-   "0009" -> cdrStrip(timeChange, Rec, ModuleFlag, 86, false, Out);
-   "0010" -> cdrStrip(switchRestart, Rec, ModuleFlag, 72, false, Out);
-   "0011" -> cdrStrip(blockHeader, Rec, ModuleFlag, 64, false, Out);
-   "0013" -> cdrStrip(incomingGateway, Rec, ModuleFlag, 248, false, Out);
-   "0014" -> cdrStrip(outgoingGateway, Rec, ModuleFlag, 248, false, Out);
-   "0015" -> cdrStrip(incomingIntraPLMNTrunk, Rec, ModuleFlag, 248, false, Out);
-   "0016" -> cdrStrip(outgoingIntraPLMNTrunk, Rec, ModuleFlag, 248, false, Out);
-   "0017" -> cdrStrip(transit, Rec, ModuleFlag, 248, false, Out);
-   "0018" -> cdrStrip(roaming, Rec, ModuleFlag, 328, false, Out);
-   "0019" -> cdrStrip(commonEquipmentUsage, Rec, ModuleFlag, 180, false, Out);
-   "0020" -> cdrStrip(acknowledgement, Rec, ModuleFlag, 206, false, Out);
-   "0021" -> cdrStrip(locationServices, Rec, ModuleFlag, 332, false, Out)
-end.
+    ModuleFlag = case lists:sublist(Rec, 9,1) of
+		     "0" -> false;
+		     "1" -> true
+		 end,
+    case lists:sublist(Rec, 10, 4) of
+	"0001" -> cdrStrip(locationUpdate, Rec, ModuleFlag, 256, false, Out);
+	"0002" -> cdrStrip(mobileOriginated, Rec, ModuleFlag, 392, true, Out);
+	"0003" -> cdrStrip(mobileTerminated, Rec, ModuleFlag, 358, true, Out);
+	"0004" -> cdrStrip(smsMobileOriginated, Rec, ModuleFlag, 302, true, Out);
+	"0005" -> cdrStrip(smsMobileTerminated, Rec, ModuleFlag, 276, true, Out);
+	"0006" -> cdrStrip(supplementaryServiceAction, Rec, ModuleFlag, 246, false, Out);
+	"0007" -> cdrStrip(tranferIn, Rec, ModuleFlag, 78, false, Out);
+	"0008" -> cdrStrip(transferOut, Rec, ModuleFlag, 98, false, Out);
+	"0009" -> cdrStrip(timeChange, Rec, ModuleFlag, 86, false, Out);
+	"0010" -> cdrStrip(switchRestart, Rec, ModuleFlag, 72, false, Out);
+	"0011" -> cdrStrip(blockHeader, Rec, ModuleFlag, 64, false, Out);
+	"0013" -> cdrStrip(incomingGateway, Rec, ModuleFlag, 248, false, Out);
+	"0014" -> cdrStrip(outgoingGateway, Rec, ModuleFlag, 248, false, Out);
+	"0015" -> cdrStrip(incomingIntraPLMNTrunk, Rec, ModuleFlag, 248, false, Out);
+	"0016" -> cdrStrip(outgoingIntraPLMNTrunk, Rec, ModuleFlag, 248, false, Out);
+	"0017" -> cdrStrip(transit, Rec, ModuleFlag, 248, false, Out);
+	"0018" -> cdrStrip(roaming, Rec, ModuleFlag, 328, false, Out);
+	"0019" -> cdrStrip(commonEquipmentUsage, Rec, ModuleFlag, 180, false, Out);
+	"0020" -> cdrStrip(acknowledgement, Rec, ModuleFlag, 206, false, Out);
+	"0021" -> cdrStrip(locationServices, Rec, ModuleFlag, 332, false, Out)
+    end.
 
 cdrStrip(Type, Record, ModuleFlag, Length, Output, Out) -> 
-  case ModuleFlag of 
-    false -> CDR = Record, Modules = [];
-    true  -> {CDR, Modules} = lists:split(Length,Record)
+    case ModuleFlag of 
+	false -> CDR = Record, Modules = [];
+	true  -> {CDR, Modules} = lists:split(Length,Record)
     end,
     Mods = parse_modules(Modules),
     if Output == true -> io:format(Out,"~s~n",[CDR]);
-      true -> []
-      end. 
+       true -> []
+    end. 
 
-    
+
 parse_modules(Modules) ->
-  cut_modules(Modules,[]).
-  
+    cut_modules(Modules,[]).
+
 cut_modules(Modules,Parsed) ->
-case lists:sublist( Modules, 2, 2) of
-  "00" -> [Parsed | {end_of_modules,lists:sublist(Modules, 4)} ];
-  "02" -> cutter(24,  Modules, Parsed, bearer_service);
-  "03" -> cutter(126, Modules, Parsed, location_and_channel);
-  "05" -> cutter(62,  Modules, Parsed, supplementary_services);
-  "06" -> cutter(24,  Modules, Parsed, teleservices);
-  "07" -> cutter(70,  Modules, Parsed, aoc_parameter);
-  "08" -> cutter(22,  Modules, Parsed, tariff_class);
-  "09" -> cutter(72,  Modules, Parsed, dataservice);
-  "10" -> cutter(20,  Modules, Parsed, other_agent);
-  "04" -> cutter(52,  Modules, Parsed, location_only);
-  "11" -> cutter(92,  Modules, Parsed, module_11);
-  "12" -> cutter(36,  Modules, Parsed, equal_access);
-  "13" -> cutter(26,  Modules, Parsed, partial);
-  "16" -> cutter(8,   Modules, Parsed, trunk_usage);
-  "18" -> cutter(224, Modules, Parsed, in_info);
-  "19" -> cutter(142, Modules, Parsed, in_charging);
-  "20" -> cutter(84,  Modules, Parsed, generic_address);
-  "21" -> cutter(54,  Modules, Parsed, module_21);
-  "22" -> cutter(50,  Modules, Parsed, network_call_reference);
-  "23" -> cutter(358, Modules, Parsed, camel_charging);
-  "25" -> cutter(34,  Modules, Parsed, mobile_number_portability);
-  "26" -> cutter(140, Modules, Parsed, gsm_assisting_ssp);
-  "27" -> cutter(214, Modules, Parsed, camel_sms);
-  "28" -> cutter(116, Modules, Parsed, patching)  
-end.
-  
+    case lists:sublist( Modules, 2, 2) of
+	"00" -> [Parsed | {end_of_modules,lists:sublist(Modules, 4)} ];
+	"02" -> cutter(24,  Modules, Parsed, bearer_service);
+	"03" -> cutter(126, Modules, Parsed, location_and_channel);
+	"05" -> cutter(62,  Modules, Parsed, supplementary_services);
+	"06" -> cutter(24,  Modules, Parsed, teleservices);
+	"07" -> cutter(70,  Modules, Parsed, aoc_parameter);
+	"08" -> cutter(22,  Modules, Parsed, tariff_class);
+	"09" -> cutter(72,  Modules, Parsed, dataservice);
+	"10" -> cutter(20,  Modules, Parsed, other_agent);
+	"04" -> cutter(52,  Modules, Parsed, location_only);
+	"11" -> cutter(92,  Modules, Parsed, module_11);
+	"12" -> cutter(36,  Modules, Parsed, equal_access);
+	"13" -> cutter(26,  Modules, Parsed, partial);
+	"16" -> cutter(8,   Modules, Parsed, trunk_usage);
+	"18" -> cutter(224, Modules, Parsed, in_info);
+	"19" -> cutter(142, Modules, Parsed, in_charging);
+	"20" -> cutter(84,  Modules, Parsed, generic_address);
+	"21" -> cutter(54,  Modules, Parsed, module_21);
+	"22" -> cutter(50,  Modules, Parsed, network_call_reference);
+	"23" -> cutter(358, Modules, Parsed, camel_charging);
+	"25" -> cutter(34,  Modules, Parsed, mobile_number_portability);
+	"26" -> cutter(140, Modules, Parsed, gsm_assisting_ssp);
+	"27" -> cutter(214, Modules, Parsed, camel_sms);
+	"28" -> cutter(116, Modules, Parsed, patching)  
+    end.
+
 cutter(Length, Modules, Parsed, Type) ->
-  {Mod, Rest} = lists:split(Length, Modules),  
-  cut_modules( Rest, [Parsed | {Type, Mod}]).
-    
-    
-    
+    {Mod, Rest} = lists:split(Length, Modules),  
+    cut_modules( Rest, [Parsed | {Type, Mod}]).
+
+
